a6e0892bcd8ea7c0eb6c91a669172922abf94a5c
José Ney Guerrero <joseneyguerrero@gmail.com>
2016-03-25 01:32:06 -0600

    added main function to generate random password and added length argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate argparse;
+extern crate rand;
+
+use rand::{random, Rng};
+use argparse::{ArgumentParser, Store};
+
+fn random_password(length:u32) -> String {
+    (0..length).map(|_| (0x20u8 + (random::<f32>() * 96.0) as u8) as char).collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut length:u32 = rand::thread_rng().gen_range(10, 21);
+    {  // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("This tool generates a random password");
+        ap.refer(&mut length)
+        .add_option(&["-l", "--length"], Store,
+        "Length of password, defaults to a random length between 10 and 20");
+        ap.parse_args_or_exit();
+    }
+    println!("Generated password of length {} => {}", length, random_password(length));
 }
